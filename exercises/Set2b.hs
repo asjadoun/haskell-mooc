a0a68cabbbb7475ad06f491213a2e5732e273380
Anil Singh <asjadoun@gmail.com>
2025-06-02 05:33:32 -0400

    Set2a done 2b wip

--- exercises/Set2b.hs
+++ exercises/Set2b.hs
@@ -16,7 +16,10 @@ import Data.List
 -- Hint! pattern matching is your friend.
 
 binomial :: Integer -> Integer -> Integer
-binomial = todo
+binomial n 0 = 1
+binomial 0 k = 0
+binomial n k = binomial (n-1) k + binomial (n-1) (k-1)
+
 
 ------------------------------------------------------------------------------
 -- Ex 2: implement the odd factorial function. Odd factorial is like
@@ -27,7 +30,10 @@ binomial = todo
 --   oddFactorial 6 ==> 5*3*1 ==> 15
 
 oddFactorial :: Integer -> Integer
-oddFactorial = todo
+oddFactorial n
+    | n <= 0 = 1
+    | even n = oddFactorial (n-1)
+    | otherwise = n * oddFactorial (n-1)
 
 ------------------------------------------------------------------------------
 -- Ex 3: implement the Euclidean Algorithm for finding the greatest
@@ -59,7 +65,11 @@ oddFactorial = todo
 -- * https://en.wikipedia.org/wiki/Euclidean_algorithm
 
 myGcd :: Integer -> Integer -> Integer
-myGcd = todo
+myGcd a b 
+    | a == 0 = b
+    | b == 0 = a
+    | a >= b = myGcd (a-b) b
+    | otherwise = myGcd a (b-a)
 
 ------------------------------------------------------------------------------
 -- Ex 4: Implement the function leftpad which adds space characters
@@ -75,7 +85,7 @@ myGcd = todo
 -- * you can compute the length of a string with the length function
 
 leftpad :: String -> Int -> String
-leftpad = todo
+leftpad s n = if length s >= n then s else leftpad ([' '] ++ s) n
 
 ------------------------------------------------------------------------------
 -- Ex 5: let's make a countdown for a rocket! Given a number, you
@@ -89,9 +99,12 @@ leftpad = todo
 -- * you can combine strings with the ++ operator
 -- * you can use the show function to convert a number into a string
 -- * you'll probably need a recursive helper function
+cdHelper :: Integer -> String -> String
+cdHelper 1 s = s ++ "1... "
+cdHelper n s = cdHelper (n-1) (s ++ show n ++ "... ")
 
 countdown :: Integer -> String
-countdown = todo
+countdown n = "Ready!" ++ cdHelper n [' '] ++ "Liftoff!"
 
 ------------------------------------------------------------------------------
 -- Ex 6: implement the function smallestDivisor that returns the
